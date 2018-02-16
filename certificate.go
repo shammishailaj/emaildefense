70d83cfcf55d25c6f629817da0349e3f61819ff6
Sebastian <s.broekhoven@gmail.com>
2018-02-16 02:31:20 +0100

    zcrypto

--- certificate.go
+++ certificate.go
@@ -1,14 +1,11 @@
 package emaildefense
 
 import (
-	"crypto/sha256"
-	"crypto/sha512"
 	"crypto/tls"
-	"crypto/x509"
-	"encoding/hex"
-	"net"
 	"net/smtp"
 	"time"
+
+	"github.com/zmap/zcrypto/x509"
 )
 
 // CertInfo struct for certificate information
@@ -34,66 +31,16 @@ type spki struct {
 	SHA512 string `json:"sha512,omitempty"`
 }
 
-func getCertificate(server string) (*certificate, error) {
-	answer := new(certificate)
-
-	smtpcert, err := runSMTP(server)
-	if err != nil {
-		// println(err)
-		answer.ConnectionError = err
-		return answer, err
-	}
-	answer.Certificate.SHA256 = sha256hash(smtpcert[0].RawTBSCertificate)
-
-	// answer.X509 = smtpcert[0]
-
-	/*
-		answer.Certificate.Raw = hex.EncodeToString(smtpcert[0].Raw)
-		answer.Certificate.SHA256 = sha256hash(smtpcert[0].Raw)
-		answer.Certificate.SHA512 = sha512hash(smtpcert[0].Raw)
-
-		answer.SPKI.Raw = hex.EncodeToString(smtpcert[0].RawSubjectPublicKeyInfo)
-		answer.SPKI.SHA256 = sha256hash(smtpcert[0].RawSubjectPublicKeyInfo)
-		answer.SPKI.SHA512 = sha512hash(smtpcert[0].RawSubjectPublicKeyInfo)
-	*/
-	answer.CommonName = smtpcert[0].Subject.CommonName
-	answer.Issuer = smtpcert[0].Issuer.CommonName
-	answer.Expires = smtpcert[0].NotAfter
-
-	return answer, nil
-}
-
-func sha256hash(cert []byte) string {
-	sh256 := sha256.New()
-	sh256.Write(cert)
-	sh256sum := hex.EncodeToString(sh256.Sum(nil))
-	return sh256sum
-}
-
-func sha512hash(cert []byte) string {
-	sh512 := sha512.New()
-	sh512.Write(cert)
-	sh512sum := hex.EncodeToString(sh512.Sum(nil))
-	return sh512sum
-}
-
-// runSMTP function for starting the check
-func runSMTP(server string) ([]*x509.Certificate, error) {
-	var (
-		err error
-	)
-
-	host, _, _ := net.SplitHostPort(server)
+func getCertificate(fqdn string) (*certificate, error) {
+	r := new(certificate)
 
 	tlsconfig := &tls.Config{
 		InsecureSkipVerify: true,
-		ServerName:         host,
+		ServerName:         fqdn,
 	}
 
-	// c, err := tls.DialWithDialer(dialer, "tcp", server, tlsconfig)
-	c, err := smtp.Dial(net.JoinHostPort(server, "25"))
+	c, err := smtp.Dial(fqdn + ":25")
 	if err != nil {
-		// fmt.Printf("[X] Dial error: %v\n", err)
 		return nil, err
 	}
 
@@ -105,5 +52,24 @@ func runSMTP(server string) ([]*x509.Certificate, error) {
 	}
 	c.Quit()
 
-	return cs.PeerCertificates, nil
+	peerChain := cs.PeerCertificates
+	if len(peerChain) == 0 {
+		return nil, err
+	}
+
+	r.X509, err = x509.ParseCertificate(peerChain[0].Raw)
+	if err != nil {
+		return nil, err
+	}
+
+	/*
+		for _, peer := range peerChain {
+			parsed, err := x509.ParseCertificate(peer.Raw)
+			if err != nil {
+				return nil, err
+			}
+			r.Parsed = append(r.Parsed, parsed)
+		}
+	*/
+	return r, err
 }
